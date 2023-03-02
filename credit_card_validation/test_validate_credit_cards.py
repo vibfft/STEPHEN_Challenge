6999a3e5ce31802d1935eeab60f759e700b757e2
Stephen Moon <stephen.moon@gmail.com>
2023-03-01 20:15:15 -0800

    added tests

--- credit_card_validation/test_validate_credit_cards.py
+++ credit_card_validation/test_validate_credit_cards.py
@@ -1,21 +1,89 @@
 import validate_credit_card
 import unittest
 
+
 class Test_TestValidate_Input(unittest.TestCase):
     def test_validate_negative_integer(self):
-        self.assertFalse(validate_credit_card.validate_input("-1", True), False)
+        self.assertFalse(
+            validate_credit_card.validate_input("-1", True), False)
 
     def test_validate_zero(self):
         self.assertFalse(validate_credit_card.validate_input("0", True), False)
 
     def test_validate_hundred(self):
-        self.assertFalse(validate_credit_card.validate_input("100", True), False)
+        self.assertFalse(
+            validate_credit_card.validate_input("100", True), False)
 
     def test_validate_integer_one(self):
         self.assertTrue(validate_credit_card.validate_input("1", True), True)
 
     def test_validate_integer_ninety_nine(self):
         self.assertTrue(validate_credit_card.validate_input("99", True), True)
+
+
+class HelperClass:
+
+    cc_numbers = ["4123456789123456",
+                  "5123-4567-8912-3456",
+                  "3123356789123456",
+                  "5123 - 3567 - 8912 - 3456",
+                  "4123 - 3567 - 8912 -_3456",
+                  "44244x4424442444",
+                  "612-3567-8912-73456",
+                  "5133-3367-8912-3456"]
+    cc_iterator = iter(cc_numbers)
+
+
+class Test_TestCreditCardNumbers(unittest.TestCase):
+
+    def test_must_only_consists_of_digits_zero_to_nine(self):
+        cc_number = next(HelperClass.cc_iterator)
+        vcc = validate_credit_card.ValidateCreditCard(
+            len(HelperClass.cc_numbers), cc_number)
+        self.assertTrue(vcc.must_only_consist_of_digits_zero_to_nine(), True)
+
+    def test_find_position_of_hyphen(self):
+        cc_number = next(HelperClass.cc_iterator)
+        vcc = validate_credit_card.ValidateCreditCard(
+            len(HelperClass.cc_numbers), cc_number)
+        self.assertTrue(vcc.find_position_of_hyphen, True)
+
+    def test_must_start_with_four_five_six(self):
+        cc_number = next(HelperClass.cc_iterator)
+        vcc = validate_credit_card.ValidateCreditCard(
+            len(HelperClass.cc_numbers), cc_number)
+        self.assertTrue(vcc.must_start_with_four_five_six(), False)
+
+    def test_separator_space_other_than_hyphen(self):
+        cc_number = next(HelperClass.cc_iterator)
+        vcc = validate_credit_card.ValidateCreditCard(
+            len(HelperClass.cc_numbers), cc_number)
+        self.assertFalse(vcc.find_position_of_hyphen(), False)
+
+    def test_separator_underscore_other_than_hyphen(self):
+        cc_number = next(HelperClass.cc_iterator)
+        vcc = validate_credit_card.ValidateCreditCard(
+            len(HelperClass.cc_numbers), cc_number)
+        self.assertTrue(vcc.find_position_of_hyphen(), True)
+
+    def test_non_digit_in_the_credit_card_input(self):
+        cc_number = next(HelperClass.cc_iterator)
+        vcc = validate_credit_card.ValidateCreditCard(
+            len(HelperClass.cc_numbers), cc_number)
+        self.assertFalse(vcc.must_only_consist_of_digits_zero_to_nine(), False)
+
+    def test_groups_of_four(self):
+        cc_number = next(HelperClass.cc_iterator)
+        vcc = validate_credit_card.ValidateCreditCard(
+            len(HelperClass.cc_numbers), cc_number)
+        self.assertTrue(vcc.find_position_of_hyphen(), False)
+
+    def test_four_consecutive_repeats(self):
+        cc_number = next(HelperClass.cc_iterator)
+        vcc = validate_credit_card.ValidateCreditCard(
+            len(HelperClass.cc_numbers), cc_number)
+        self.assertTrue(vcc.four_consecutive_repeats(), False)
+
 
 if __name__ == '__main__':
     unittest.main()
--- credit_card_validation/validate_credit_card.py
+++ credit_card_validation/validate_credit_card.py
@@ -10,7 +10,8 @@ class ValidateCreditCard:
 
     credit_card_nums = []
     all_namedtuples = []
-    equal_groups_of_four = [4,9,14] # compare to see whether two lists are equal with == operator
+    # compare to see whether two lists are equal with == operator
+    equal_groups_of_four = [4, 9, 14]
 
     def __init__(self, total_credit_cards: int, credit_card_num: str) -> None:
         self.total_credit_cards = total_credit_cards
@@ -23,7 +24,7 @@ class ValidateCreditCard:
         if len(digits_only) == 16:  # credit card number length is required to be 16
             return True
         return False
-    
+
     def four_consecutive_repeats(self):
         for each_digit in self.digits_only:
             four_repeat = each_digit*4
@@ -37,16 +38,17 @@ class ValidateCreditCard:
         if m:
             return True
         return False
-    
+
     def find_position_of_hyphen(self):
         matched_pos_list = []
-        for match in re.finditer('-',self.credit_card_num):
+        for match in re.finditer('-', self.credit_card_num):
             matched_pos_list.append(match.start())
-        
+
         # if there are no matched hyphen or hyphen divides the number string in a group of 4
         if len(matched_pos_list) == 0 or matched_pos_list == ValidateCreditCard.equal_groups_of_four:
             return True
         return False
+
 
 def validate_input(total_credit_cards: str, debug: bool = False) -> bool:
     total_credit_cards_re = re.compile(r"\d+")
@@ -121,16 +123,18 @@ def read_credit_cards(total_credit_cards: str, debug: bool = False) -> list:
             print(each_tuple.start_with_456, end=",")
             print(each_tuple.four_consecutive_repeats, end="\n")
         if each_tuple.space and \
-            each_tuple.underscore and \
-            each_tuple.hyphen and \
-            each_tuple.all_zero_nine and \
-            each_tuple.start_with_456 and \
-            each_tuple.four_consecutive_repeats:
-            
-            if debug: print(each_tuple.line, end=":")
+                each_tuple.underscore and \
+                each_tuple.hyphen and \
+                each_tuple.all_zero_nine and \
+                each_tuple.start_with_456 and \
+                each_tuple.four_consecutive_repeats:
+
+            if debug:
+                print(each_tuple.line, end=":")
             print("Valid")
         else:
-            if debug: print(each_tuple.line, end=":")
+            if debug:
+                print(each_tuple.line, end=":")
             print("Invalid")
 
 
@@ -138,6 +142,7 @@ def main() -> None:
 
     total_credit_cards = input(
         "Enter number of credit cards to verify and then input credit card numbers:\n")
+    
     debug = False
     if validate_input(total_credit_cards, debug):
         if debug:
